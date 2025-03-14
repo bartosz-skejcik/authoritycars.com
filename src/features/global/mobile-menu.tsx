22874505c5ef9737c569c744b21b3886f4673699
Bartłomiej Paczesny <bartek@paczesny.pl>
2025-03-14 12:42:23 +0100

    fix

--- src/features/global/mobile-menu.tsx
+++ src/features/global/mobile-menu.tsx
@@ -10,53 +10,58 @@ import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
 type MobileMenuProps = {
-  links: { name: string; href: string }[];
+    links: { name: string; href: string }[];
 };
 
 export default function MobileMenu({ links }: MobileMenuProps) {
-  const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(false);
 
-  return (
-    <Sheet open={open} onOpenChange={setOpen}>
-      <SheetTrigger asChild>
-        <Button variant="ghost" size="icon" className="lg:hidden">
-          <Menu className="h-5 w-5" />
-          <span className="sr-only">Toggle menu</span>
-        </Button>
-      </SheetTrigger>
-      <SheetContent side="right" className="flex flex-col justify-between p-0">
-        <div className="flex flex-col p-6">
-          <div className="flex items-center justify-between">
-            <Link
-              href="/"
-              className="flex items-center gap-2"
-              onClick={() => setOpen(false)}
+    return (
+        <Sheet open={open} onOpenChange={setOpen}>
+            <SheetTrigger asChild>
+                <Button variant="ghost" size="icon" className="lg:hidden">
+                    <Menu className="h-5 w-5" />
+                    <span className="sr-only">Toggle menu</span>
+                </Button>
+            </SheetTrigger>
+            <SheetContent
+                side="right"
+                className="flex flex-col justify-between p-0"
             >
-              <Image
-                src="/logo.jpg"
-                alt="Authoritycars logo"
-                width={150}
-                height={50}
-                className="aspect-square w-8 rounded-xl"
-              />
-              <h2 className="text-lg font-bold italic">authoritycars.com</h2>
-            </Link>
-          </div>
-          <div className="mt-8 flex flex-col gap-6">
-            <NavbarItems
-              links={links}
-              className="flex-col items-start gap-6"
-              itemClassName="text-lg"
-              onClick={() => setOpen(false)}
-            />
-          </div>
-        </div>
-        <div className="border-t p-6">
-          <div className="flex flex-col gap-6">
-            <Socials variant="accent" size="sm" />
-          </div>
-        </div>
-      </SheetContent>
-    </Sheet>
-  );
+                <div className="flex flex-col p-6">
+                    <div className="flex items-center justify-between">
+                        <Link
+                            href="/"
+                            className="flex items-center gap-2"
+                            onClick={() => setOpen(false)}
+                        >
+                            <Image
+                                src="/logo.jpg"
+                                alt="Authoritycars logo"
+                                width={150}
+                                height={50}
+                                className="aspect-square w-8 rounded-xl"
+                            />
+                            <h2 className="text-lg font-bold italic">
+                                authoritycars.pl
+                            </h2>
+                        </Link>
+                    </div>
+                    <div className="mt-8 flex flex-col gap-6">
+                        <NavbarItems
+                            links={links}
+                            className="flex-col items-start gap-6"
+                            itemClassName="text-lg"
+                            onClick={() => setOpen(false)}
+                        />
+                    </div>
+                </div>
+                <div className="border-t p-6">
+                    <div className="flex flex-col gap-6">
+                        <Socials variant="accent" size="sm" />
+                    </div>
+                </div>
+            </SheetContent>
+        </Sheet>
+    );
 }
--- src/features/global/navbar.tsx
+++ src/features/global/navbar.tsx
@@ -7,73 +7,80 @@ import { cn } from "@/lib/utils";
 import MobileMenu from "./mobile-menu";
 
 const links = [
-  //{
-  //  name: "Opinie klientów",
-  //  href: "/#opinie",
-  //},
-  {
-    name: "Zostań partnerem",
-    href: "/#partner",
-  },
-  {
-    name: "Popularne pytania",
-    href: "/faq",
-  },
-  {
-    name: "Formularz zgłoszeniowy",
-    href: "/#formularz",
-  },
+    //{
+    //  name: "Opinie klientów",
+    //  href: "/#opinie",
+    //},
+    {
+        name: "Zostań partnerem",
+        href: "/#partner",
+    },
+    {
+        name: "Popularne pytania",
+        href: "/faq",
+    },
+    {
+        name: "Formularz zgłoszeniowy",
+        href: "/#formularz",
+    },
 ];
 
 function Navbar({ className }: { className?: string }) {
-  return (
-    <>
-      {/* Mobile Navbar */}
-      <nav
-        className={cn(
-          "bg-card border-input sticky top-0 z-50 flex h-16 w-full items-center justify-between border-b px-4 py-2 lg:hidden",
-          className,
-        )}
-      >
-        <Link href="/" className="flex items-center gap-2">
-          <Image
-            src="/logo.jpg"
-            alt="Authoritycars logo"
-            width={150}
-            height={50}
-            className="aspect-square w-8 rounded-xl"
-          />
-          <h2 className="text-lg font-bold italic">authoritycars.com</h2>
-        </Link>
+    return (
+        <>
+            {/* Mobile Navbar */}
+            <nav
+                className={cn(
+                    "bg-card border-input sticky top-0 z-50 flex h-16 w-full items-center justify-between border-b px-4 py-2 lg:hidden",
+                    className
+                )}
+            >
+                <Link href="/" className="flex items-center gap-2">
+                    <Image
+                        src="/logo.jpg"
+                        alt="Authoritycars logo"
+                        width={150}
+                        height={50}
+                        className="aspect-square w-8 rounded-xl"
+                    />
+                    <h2 className="text-lg font-bold italic">
+                        authoritycars.pl
+                    </h2>
+                </Link>
 
-        <MobileMenu links={links} />
-      </nav>
+                <MobileMenu links={links} />
+            </nav>
 
-      {/* Desktop Sidebar */}
-      <nav
-        className={cn(
-          "bg-card border-input hidden h-screen w-full flex-col items-center justify-between gap-5 border-b px-3 py-3 lg:flex",
-          className,
-        )}
-      >
-        <Link href="/" className="flex items-center justify-center gap-2">
-          <Image
-            src="/logo.jpg"
-            alt="Authoritycars logo"
-            width={150}
-            height={50}
-            className="aspect-square w-10 rounded-xl"
-          />
-          <h2 className="mb-1 text-xl font-bold italic">authoritycars.com</h2>
-        </Link>
-        <NavbarItems links={links} />
-        <div className="flex w-full flex-col items-center justify-center gap-12">
-          <Socials variant="accent" size="sm" />
-          <ProfileDropdown />
-        </div>
-      </nav>
-    </>
-  );
+            {/* Desktop Sidebar */}
+            <nav
+                className={cn(
+                    "bg-card border-input hidden h-screen w-full flex-col items-center justify-between gap-5 border-b px-3 py-3 lg:flex",
+                    className
+                )}
+            >
+                <Link
+                    href="/"
+                    className="flex items-center justify-center gap-2"
+                >
+                    <Image
+                        src="/logo.jpg"
+                        alt="Authoritycars logo"
+                        width={150}
+                        height={50}
+                        className="aspect-square w-10 rounded-xl"
+                    />
+                    <h2 className="mb-1 text-xl font-bold italic">
+                        authoritycars.pl
+                    </h2>
+                </Link>
+                <NavbarItems links={links} />
+                <div className="flex w-full flex-col items-center justify-center gap-12">
+                    <Socials variant="accent" size="sm" />
+                    <ProfileDropdown />
+                </div>
+            </nav>
+        </>
+    );
 }
 
 export default Navbar;
